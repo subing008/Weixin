70a559c18ebac1e24338b60f6e30626b27dd3ee5
subing008 <275120420@qq.com>
2016-11-23 17:10:11 +0800

    使用wechat验证微信
    使用wechat验证微信

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var xmlparser = require('express-xml-bodyparser');
+//var xmlparser = require('express-xml-bodyparser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+ var weixin = require('./routes/weixin');
 
 var app = express();
 
@@ -21,11 +22,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(xmlparser());
+//app.use(xmlparser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
+ app.use(express.query());
+ app.use('/weixin', weixin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/weixin.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var wechat = require('wechat');
+var token = "79faf82271944fe38c4f1d99be71bc9c";
+
+
+router.get('/',wechat(token).middlewarify());
+router.post('/', wechat(token).text(function(message,req,res,next){
+  if (message.Content === "帮助") {
+    res.reply("当前公众号正在开发阶段，更多功能敬请期待！");
+  }else{
+    res.reply("本公众号测试专用！");
+  }
+}).middlewarify());
+
+module.exports = router;
