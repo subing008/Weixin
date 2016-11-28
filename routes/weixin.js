ac8a0c7b8beeaa93e0743ba54affad3feb9a058d
subing008 <275120420@qq.com>
2016-11-28 23:06:14 +0800

    添加accessToken测试借口

--- routes/weixin.js
+++ routes/weixin.js
@@ -2,14 +2,27 @@ var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
 var jssdk = require('../lib/jssdk.js');
-
 var token = "79faf82271944fe38c4f1d99be71bc9c";
-
 
 router.get('/',wechat(token).middlewarify());
 router.post('/', wechat(token).text(function(message,req,res,next){
   if (message.Content === "帮助") {
     res.reply("当前公众号正在开发阶段，更多功能敬请期待！");
+  }else if(message.Content === "ac"){
+    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
+    js.getAccessToken(function(err,accessToken){
+      res.reply("accessToken : " + accessToken);
+    })
+  }else if(message.Content === "js"){
+    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
+    js.getJsApiTicket(function(err,JsApiTicket){
+      res.reply("JsApiTicket : " + JsApiTicket);
+    })
+  }else if(message.Content === "sp"){
+    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
+    js.getSignPackage("http://subing.duapp.com/weixin",function(err,SignPackage){
+      res.reply("SignPackage : " + JSON.stringify(SignPackage));
+    })
   }else{
     res.reply("本公众号测试专用！");
   }
