59edb8e654ded80a9a4603b0c5f96006f31d2028
subing008 <275120420@qq.com>
2016-11-29 18:44:32 +0800

    微信公众号内嵌网页测试1
    微信公众号内嵌网页测试1

--- routes/weixin.js
+++ routes/weixin.js
@@ -4,23 +4,34 @@ var wechat = require('wechat');
 var jssdk = require('../lib/jssdk.js');
 var token = "79faf82271944fe38c4f1d99be71bc9c";
 
+router.get('/user',function(req,res,next){
+  jssdk.getSignPackage(req.url,function(err,SignPackage){
+    if (err) {
+      return next(err);
+    }
+
+    res.render('index',{
+      title: "微信公众平台测试页面",
+      signPackage: JSON.stringify(SignPackage),
+      pretty: true,
+    })
+  });
+});
+
 router.get('/',wechat(token).middlewarify());
 router.post('/', wechat(token).text(function(message,req,res,next){
   if (message.Content === "帮助") {
     res.reply("当前公众号正在开发阶段，更多功能敬请期待！");
   }else if(message.Content === "ac"){
-    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
-    js.getAccessToken(function(err,accessToken){
+    jssdk.getAccessToken(function(err,accessToken){
       res.reply("accessToken : " + accessToken);
     })
   }else if(message.Content === "js"){
-    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
-    js.getJsApiTicket(function(err,JsApiTicket){
+    jssdk.getJsApiTicket(function(err,JsApiTicket){
       res.reply("JsApiTicket : " + JsApiTicket);
     })
   }else if(message.Content === "sp"){
-    var js = new jssdk("wxc91966ee5d3eaebc","bba039ca1a84ce1816601507120fb9ff");
-    js.getSignPackage("http://subing.duapp.com/weixin",function(err,SignPackage){
+    jssdk.getSignPackage("http://subing.duapp.com/weixin",function(err,SignPackage){
       res.reply("SignPackage : " + JSON.stringify(SignPackage));
     })
   }else{
